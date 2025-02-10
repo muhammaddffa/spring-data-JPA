b875f5b0d2c55b5b09058d1e058d11c4ce391ef1
muhammaddffa <muhammaddaffa463@gmail.com>
2025-02-10 18:31:18 +0700

    Query Method

---
+++ src/main/java/spring_data_jpa/belajar_spring_data_jpa/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package spring_data_jpa.belajar_spring_data_jpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import spring_data_jpa.belajar_spring_data_jpa.entity.Product;
+
+import java.util.List;
+
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByCategory_Name(String name);
+}
---
+++ src/test/java/spring_data_jpa/belajar_spring_data_jpa/repository/ProductRepositoryTest.java
@@ -0,0 +1,52 @@
+package spring_data_jpa.belajar_spring_data_jpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import spring_data_jpa.belajar_spring_data_jpa.entity.Category;
+import spring_data_jpa.belajar_spring_data_jpa.entity.Product;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void createProduct(){
+        Category category = categoryRepository.findById(1L).orElse(null);
+        assertNotNull(category);
+
+        {
+            Product product = new Product();
+            product.setName("Apple iPhone 14 Pro Max");
+            product.setPrice(25_000_000L);
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+
+        {
+            Product product = new Product();
+            product.setName("Apple iPhone 13 Pro Max");
+            product.setPrice(18_000_000L);
+            product.setCategory(category);
+            productRepository.save(product);
+        }
+    }
+
+    @Test
+    void findByCategoryName(){
+        List<Product> products = productRepository.findAllByCategory_Name("GADGET MURAH");
+        assertEquals(2, products.size());
+        assertEquals("Apple iPhone 14 Pro Max", products.get(0).getName());
+        assertEquals("Apple iPhone 13 Pro Max", products.get(1).getName());
+    }
+}
